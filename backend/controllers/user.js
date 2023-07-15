824f896547833065050c926c8ce73f52e55c889a
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-07-15 16:51:26 -0700

    ⚡️ Refactoring

---
+++ backend/controllers/user.js
@@ -0,0 +1,75 @@
+//  ユーザーの新規作成とパスワードの暗号化を行うファイル
+
+const CryptoJS = require("crypto-js");
+const JWT = require("jsonwebtoken");
+
+// const User = require("./models/User");
+const newUser = require("../models/newUser");
+
+exports.register = async (req, res) => {
+  // Receive Password
+  const password = req.body.password;
+
+  try {
+    // encrypt password
+    req.body.password = CryptoJS.AES.encrypt(password, process.env.SECRET_KEY);
+
+    // Create new user to mongo DB
+    const user = await newUser.create(req.body);
+
+    // Publish JWT
+    // mongoにあるidを元にtokenを発行していく
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+    return res.status(200).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+// User login API
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // DBからユーザーが存在するか探してくる
+    const user = await newUser.findOne({ username: username });
+    if (!user) {
+      return res.status(401).json({
+        errors: {
+          param: "username",
+          message: "Invalid username",
+        },
+      });
+    }
+
+    // パスワードが合っているか照合する
+    // 暗号化されたパスワードを複合化する必要がある
+    const descryptedPassword = CryptoJS.AES.decrypt(
+      // パスワードは、user変数の中に存在するので下記の書き方にしている
+      user.password,
+      process.env.SECRET_KEY
+    ).toString(CryptoJS.enc.Utf8);
+    // .toString(CryptoJS.enc.Utf8);で複合したパスワードを文字列として認識することができる
+
+    // descryptedPasswordが入力されたパスワードと合っていない場合エラーになる
+    if (descryptedPassword !== password) {
+      return res.status(401).json({
+        errors: {
+          param: "password",
+          message: "Invalid password",
+        },
+      });
+    }
+
+    // JWTを発行する
+    const token = JWT.sign({ id: user._id }, process.env.TOKEN_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+
+    return res.status(201).json({ user, token });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,24 @@
 const express = require("express");
 const app = express();
-const userRoute = require("./routes/users");
-const authRoute = require("./routes/auth");
-const postRoute = require("./routes/posts");
-const movieRoute = require("./routes/movie");
 const PORT = 8080;
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// Connect to Database
-// 接続ができないので確認する
-mongoose
-  .connect(process.env.MONGOURL)
-  .then(() => {
-    console.log("Connecting to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-// middle ware
 app.use(express.json());
-app.use("/api/users", userRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/movie", movieRoute);
+// エンドポイントの準備を行う
+// localhost:8080/api/registerのエンドポイントになる
+// 第二引数にそれに適用するパスを指定する
+app.use("/api", require("./routes/auth"));
+
+// DB接続
+try {
+  mongoose.connect(process.env.MONGOURL);
+  console.log("Connect to DB");
+} catch (error) {
+  console.log(error);
+}
 
 app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`);
+  console.log(`Server is working ${PORT}`);
 });
