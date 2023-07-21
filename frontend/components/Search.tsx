671049fe3d579e933c08798860e16bf481045db4
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-07-20 19:30:50 -0700

    ⚡️

--- frontend/components/Search.tsx
+++ frontend/components/Search.tsx
@@ -8,53 +8,47 @@ interface films {
   title: string;
 }
 
-const apikey = process.env.API_KEY;
-const searchApi = process.env.SEARCH_MOVIES;
+const API_KEY = "bb46848237eacc0a36827f6639b47ee3";
+const SEARCH_KEY = process.env.SEARCH_MOVIES;
 const img = process.env.IMG;
 
 const Search = () => {
-  const URL = img; // ポスター画像のベースURL
+  const URL = "https://image.tmdb.org/t/p/w500";
   // https://developer.themoviedb.org/reference/movie-changesどこにsearchのurlがあるのか
   // const API_SEARCH =
   //   "https://api.themoviedb.org/3/search/movie?api_key=bb46848237eacc0a36827f6639b47ee3&query";
 
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
-  const [searchResults, setSearchResults] = useState<films[]>([]);
+  // const [searchResults, setSearchResults] = useState<films[]>([]);
 
-  // const fetchMovies = async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://api.themoviedb.org/3/movie/now_playing?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&region=US&page=1"
-  //     );
-  //     setMovies(response.data.results);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   fetchMovies();
-  // }, []);
-
-  // const searchMovies = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   try {
-  //     const response = await axios.get(
-  //       "https://api.themoviedb.org/3/movie/now_playing?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&region=US&page=1"
-  //     );
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-  const searchMovies = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const searchMovies = async () => {
     try {
       const response = await axios.get(
-        `${searchApi}?api_key=${apikey}&query=${searchValue}`
+        `https://api.themoviedb.org/3/search/movie?query=${searchValue}&api_key=${API_KEY}`
+        // "https://api.themoviedb.org/3/movie/now_playing?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&region=US&page=1"
       );
-      setSearchResults(response.data.results);
+      setMovies(response.data.results);
+      console.log(response.data.results);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  useEffect(() => {
+    searchMovies();
+  }, []);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+  console.log(setSearchValue);
+
+  const handleClick = (e: any) => {
+    searchMovies();
+
+    console.log(API_KEY);
+    console.log(searchMovies());
   };
 
   return (
@@ -84,14 +78,14 @@ const Search = () => {
           autoComplete="off"
           sx={{ width: "500px" }}
           value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
+          onChange={handleSearch}
         />
-        <button onClick={searchMovies}>Search</button>
+        <button onClick={handleClick}>Search</button>
       </Box>
       <Box sx={{ mt: "30px" }}>
         <div>
           <div style={{ display: "flex" }}>
-            {searchResults.map((movie: films) => (
+            {movies.map((movie: films) => (
               <div key={movie.id}>
                 <img src={`${URL}${movie.poster_path}`} alt={movie.title} />
               </div>
