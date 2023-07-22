11f5b3cf5c5bee5d00e7938231f0c5b4034460ae
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-07-22 15:39:40 -0700

    🎉 tv_series

--- frontend/components/MainImage.tsx
+++ frontend/components/MainImage.tsx
@@ -4,53 +4,48 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import { Box } from "@mui/material";
 
-// interfaceを使い回して良いのか
-interface films {
+interface Series {
   id: string;
   poster_path: string;
   title: string;
   original_title: string;
   release_date: string;
   vote_average: number;
-  backdrop_path: "string";
+  backdrop_path: string;
 }
 
 const PopularMovies = () => {
   const URL = "https://image.tmdb.org/t/p/w500"; // ポスター画像のベースURL
 
-  const [movies, setMovies] = useState<films[]>([]);
+  const [dramas, setDramas] = useState<Series[]>([]);
 
-  const fetchMovies = async () => {
+  const fetchDramas = async () => {
     try {
       const response = await axios.get(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&region=US&page=1"
+        "https://api.themoviedb.org/3/trending/tv/day?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&region=US&page=1"
       );
-      setMovies(response.data.results);
+      setDramas(response.data.results);
       console.log(response.data.results);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const extractYearFromDate = (dateString: string): string => {
-    return dateString.substring(0, 4); // Extract the first 4 characters (the year)
-  };
-
   useEffect(() => {
-    fetchMovies();
+    fetchDramas();
   }, []);
 
-  console.log(movies);
+  console.log(dramas);
 
   return (
     <div style={{}}>
       {/* Swiperコンポーネント */}
       <Swiper slidesPerView="auto" grabCursor={true} direction="horizontal">
-        {movies.map((movie: films) => (
-          <SwiperSlide key={movie.id}>
+        {dramas.map((drama: Series) => (
+          <SwiperSlide key={drama.id}>
             <div
               style={{
-                backgroundImage: `URL(${URL}${movie.backdrop_path})`,
+                backgroundImage: `url(${URL}${drama.backdrop_path})`,
                 backgroundSize: "cover",
                 backgroundRepeat: "no-repeat",
                 height: "100vh",
@@ -68,9 +63,9 @@ const PopularMovies = () => {
                     "linear-gradient(to right, rgb(0, 0, 0), rgba(0, 0, 0, 0))",
                 }}
               ></div>
-              <div>{movie.vote_average}</div>
-              <div>{extractYearFromDate(movie.release_date)}</div>
-              <div>{movie.original_title}</div>
+              <div>{drama.vote_average}</div>
+              {/* <div>{extractYearFromDate(movie.release_date)}</div> */}
+              <div>{drama.original_title}</div>
             </div>
           </SwiperSlide>
         ))}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,10 +2,6 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import MainImage from "@/components/MainImage";
-import PopularMovies from "@/components/PopularMovies";
-import PopularMSeries from "@/components/PopularSeries";
-import TopRatedMovies from "@/components/TopRatedMovies";
-import TopRatedSeries from "@/components/TopRatedSeries";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,10 +21,6 @@ export default function Home() {
 
       <Header />
       <MainImage />
-      <PopularMovies />
-      <PopularMSeries />
-      <TopRatedMovies />
-      <TopRatedSeries />
     </>
   );
 }
