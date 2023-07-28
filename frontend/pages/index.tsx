8c23687c924538e550233ba35046d88583d95916
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-07-27 22:50:07 -0700

    ⚡️

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,24 +2,13 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 
 import Header from "@/components/Header";
-
-
-import MainImage from "@/components/MainImage";
-
 import SingleMovieMain from "@/components/SingleMovieMain";
 import Videos from "@/components/Videos";
 import Recommend from "@/components/Recommend";
 import Movies from "@/components/Movies";
 
-
-
-import Search from "@/components/Search";
-
-
-import MainImage from "@/components/MainImage";
 import Content from "@/components/container/Content";
-
-
+import MainImage from "@/components/MainImage";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,22 +21,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Header />
       <MainImage />
       <Content />
-
-
-      <SingleMovieMain />
+      {/* <SingleMovieMain />
       <Recommend />
       <Videos />
-
-      <Movies />
-
-      <Search />
-
-
-
-
+      <Movies /> */}
+      {/* <Search /> */}
+    </>
   );
 }
---
+++ frontend/pages/search/search.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import Search from "@/components/Search";
+import React from "react";
+
+const search = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+    </div>
+  );
+};
+
+export default search;
