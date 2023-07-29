2960f05c38578e19a71f639db35d2a06aed4d11b
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-07-28 21:06:47 -0700

    ⚡️

--- frontend/components/container/PopularMovies.tsx
+++ frontend/components/container/PopularMovies.tsx
@@ -15,6 +15,8 @@ const PopularMovies = () => {
   const URL = "https://image.tmdb.org/t/p/w780"; // ポスター画像のベースURL
 
   const [movies, setMovies] = useState([]);
+  // hoverされたら発火するようにする
+  const [ishover, setIshover] = useState(false);
 
   const fetchMovies = async () => {
     try {
@@ -22,7 +24,7 @@ const PopularMovies = () => {
         "https://api.themoviedb.org/3/movie/now_playing?api_key=bb46848237eacc0a36827f6639b47ee3&language=en-US&region=US&page=1"
       );
       setMovies(response.data.results);
-      console.log(response.data.results);
+      // console.log(response.data.results);
     } catch (error) {
       console.log(error);
     }
@@ -49,14 +51,21 @@ const PopularMovies = () => {
     fetchMovies();
   }, []);
 
-  console.log(URL);
   return (
     <div>
       <h1>POPULAR MOVIES</h1>
       <Swiper slidesPerView={4} grabCursor={true} direction="horizontal">
         {movies.map((movie: films) => (
           <SwiperSlide key={movie.id}>
-            <Box sx={boxSX}>
+            <Box
+              onMouseEnter={() => {
+                setIshover(true);
+              }}
+              onMouseLeave={() => {
+                setIshover(false);
+              }}
+              sx={boxSX}
+            >
               <img
                 style={{
                   width: "100%",
@@ -66,10 +75,11 @@ const PopularMovies = () => {
                 src={`${URL}${movie.poster_path}`}
                 alt={movie.title}
               />
-
-              <div>{movie.vote_average}</div>
-              <div>{extractYearFromDate(movie.release_date)}</div>
-              <div>{movie.title}</div>
+              <Box>
+                <div>{movie.vote_average}</div>
+                <div>{extractYearFromDate(movie.release_date)}</div>
+                <div>{movie.title}</div>
+              </Box>
             </Box>
           </SwiperSlide>
         ))}
