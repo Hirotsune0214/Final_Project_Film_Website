19396919c96e08895cd619c20ba2de191215fb6d
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-08-04 19:25:49 -0700

    ⚡️ JWT worked

--- frontend/pages/signup/signup.tsx
+++ frontend/pages/signup/signup.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Signup from "@/components/Signup";
+import WithAuth from "@/components/WithAuth";
 
 const signup = () => {
   return (
-    <div>
+    <WithAuth>
       <Signup />
-    </div>
+    </WithAuth>
   );
 };
 
