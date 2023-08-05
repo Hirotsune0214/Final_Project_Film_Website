9dca46836ef86ece4e428e9ad99969192ae2634d
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-08-04 19:23:11 -0700

    ⚡️

--- frontend/components/WithAuth.tsx
+++ frontend/components/WithAuth.tsx
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { ReactNode, useEffect } from "react";
 import authUtils from "../utils/authUtils";
-import Login from "./Login";
-import Signup from "./Signup";
 
-export default function WithAush() {
+export default function WithAuth({ children }: any) {
   const router = useRouter();
+
+  // console.log("sssss");
 
   // userがloginとsignupに訪れる度に発火するuseEffect
   useEffect(() => {
@@ -13,20 +13,18 @@ export default function WithAush() {
     // JWTを持っているのか確認する。
     const checkAuth = async () => {
       // 認証チェック -> utilsファイルのauthUtilsに作成
-      // 権限があるかのチェック 
+
+      // 権限があるかのチェック
       const isAuth = await authUtils.isAuthenticated();
       // trueならリダイレクトする構文
       if (isAuth) {
         router.push("/");
+
+        console.log(isAuth, "isAuth");
       }
     };
     checkAuth();
   }, [router]);
 
-  return (
-    <div>
-      <Signup />
-      <Login />
-    </div>
-  );
+  return <div>{children}</div>;
 }
