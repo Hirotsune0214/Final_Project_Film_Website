fe3ac40eeb8b29c4a5114363db359f4e47b2577d
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-08-05 17:08:25 -0700

    Implemented Header bgColor and modified files name

--- frontend/pages/search/index.tsx
+++ frontend/pages/search/index.tsx
@@ -1,13 +1,13 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Search from "@/components/Search";
+// import Search from "@/components/Search";
 import React from "react";
 
 const search = () => {
   return (
     <div>
       <Header />
-      <Search />
+      {/* <Search /> */}
       <Footer />
     </div>
   );
