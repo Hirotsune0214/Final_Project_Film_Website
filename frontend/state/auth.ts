8246d8ca4259898af5ecdc07b716d149d9b904a1
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-08-26 20:40:22 -0700

    🔥

--- frontend/state/auth.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-// TODO: tsの型の確認
-export const usersState = atom<string | number>({
-  key: "usersState", // unique ID (with respect to other atoms/selectors)
-  // TODO: ユーザーの情報をmongodbから取得してこないといけない可能性(+)
-  default: "", // default value (aka initial value)
-});
