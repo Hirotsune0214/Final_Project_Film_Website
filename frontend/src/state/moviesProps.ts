0501044fb04c138c90ffea2c6f72d3cb2d940cd6
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-08-31 16:04:30 -0700

    🔥 Delete files

--- frontend/src/state/moviesProps.ts
+++
@@ -1,8 +0,0 @@
-// type MoviesProps = {
-//   movies: string[];
-//   movieLists: string;
-//   setMovies: string[];
-//   setMovieLists: (category: string) => void;
-// };
-
-// export default MoviesProps;
