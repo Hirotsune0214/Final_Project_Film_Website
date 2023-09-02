227804c839ff28cee624f1b6ebd295fea168bc0c
Hirotsune0214 <hiroyukitsune4@gmail.com>
2023-09-02 15:35:39 -0700

    🏷️ Added types

--- frontend/src/state/category.ts
+++ frontend/src/state/category.ts
@@ -1,19 +1,22 @@
 export type Category = {
+  key: string | undefined;
   id: string;
   poster_path: string;
+  profile_path: string;
   vote_average: number;
   overview: string;
   backdrop_path: string;
+  original_title: string;
+  name: string;
 };
 
 export type Movie = Category & {
   title: string;
   release_date: string;
+  file_path: string;
 };
 
 export type Drama = Category & {
-  name: string;
-  original_title: string;
   first_air_date: string;
 };
 
@@ -23,4 +26,10 @@ export type Searching = Category & {
   first_air_date: string;
   original_name: string;
   profile_path: string;
+};
+
+export type Recommend = Category & {
+  title: string;
+  first_air_date: string;
+  release_date: string;
 };
